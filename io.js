2ff0e5b02969d62c12d4f0d40e693b682a1eaa02
Bryant <cabrera.bryant@gmail.com>
2019-03-11 22:23:27 -0700

    Adds Join Room instructions to index.ejs view.

--- io.js
+++ io.js
@@ -3,9 +3,9 @@ const io = require("socket.io")();
 const connectCounter = 0;
 
 io.on('connection', function (socket) {
-    socket.on('connect', function () {
-        connectCounter++;
-    });
+    // socket.on('connect', function () {
+    //     connectCounter++;
+    // });
 
     socket.on('new-game', function () {
         io.emit('new-game');
@@ -15,9 +15,9 @@ io.on('connection', function (socket) {
       io.emit("move", data);
     });
 
-    socket.on("disconnect", function () {
-      connectCounter--;
-    });
+    // socket.on("disconnect", function () {
+    //   connectCounter--;
+    // });
 });
 
 module.exports = io;
